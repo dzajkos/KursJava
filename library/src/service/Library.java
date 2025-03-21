c1752ef028adfa28e1f6829c54679bdd2d0c989f
dzajkos <damianzajko@gmail.com>
2025-03-21 12:14:40 +0100

    + pozmienianko na streamy

--- library/src/service/Library.java
+++ library/src/service/Library.java
@@ -7,51 +7,45 @@ import model.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Library {
 
     private List<Item> items = new ArrayList<>();
 
     public void printAvailableItems() {
-        boolean flag = false;
-        for (Item item : items) {
-            if (!item.isRented) {
-                flag = true;
-                System.out.println(item.getDescription());
-            }
-        }
-        if (flag) {
+        List<String> descriptionsList = items.stream()
+                .filter(item -> !item.isRented)
+                .map(Item::getDescription)
+                .toList();
+        if(descriptionsList.isEmpty()) {
             System.out.println("Brak dostępnych tytułów");
+        } else {
+            descriptionsList.forEach(System.out::println);
         }
-
     }
 
     public void printRentedItems() {
-        boolean flag = false;
-        for (Item item : items) {
-            if (item.isRented) {
-                flag = true;
-                System.out.println(item.getDescription());
-            }
-        }
-        if (flag) {
-            System.out.println("Brak wypożyczonych tytułow");
+        List<String> descriptionsList = items.stream()
+                .filter(item -> item.isRented)
+                .map(Item::getDescription)
+                .toList();
+        if(descriptionsList.isEmpty()) {
+            System.out.println("Brak wypożyczonych tytułów");
+        } else {
+            descriptionsList.forEach(System.out::println);
         }
     }
 
     public void rentItem(String itemTitle) throws ItemNotFoundException {
-        Item item = findByTitle(itemTitle);
-        if (item == null) {
-            throw new ItemNotFoundException("Nie ma takiego tyutłu");
-        }
+        Item item = findByTitle(itemTitle)
+                .orElseThrow(() -> new ItemNotFoundException("Nie ma takiego tytułu"));
         item.rent();
     }
 
     public void returnItem(String itemTitle) throws ItemNotFoundException {
-        Item item = findByTitle(itemTitle);
-        if (item == null) {
-            throw new ItemNotFoundException("Nie ma takiego tyutłu");
-        }
+        Item item = findByTitle(itemTitle)
+                .orElseThrow(() -> new ItemNotFoundException("Nie ma takiego tytułu"));
         item.returnItem();
     }
 
@@ -64,13 +58,10 @@ public class Library {
         }
     }
 
-    public Item findByTitle(String itemTitle) {
-        for (Item item : items) {
-            if (item.getTitle().equals(itemTitle)) {
-                return item;
-            }
-        }
-        return null;
+    public Optional<Item> findByTitle(String itemTitle) {
+        return items.stream()
+                .filter(item -> item.getTitle().equals(itemTitle))
+                .findFirst();
     }
 
 }
